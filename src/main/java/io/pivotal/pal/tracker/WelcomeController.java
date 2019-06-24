c0708dd4aefd96dbe1dfb21e4e768dced461f170
training <249100089@qq.com>
2019-06-24 16:10:36 +0800

    commit

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,23 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
+    private final String  welcomeStr;
+
+    public WelcomeController(@Value("${welcome.message}") String welcomeStr){
+this.welcomeStr = welcomeStr;
+    }
 
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return welcomeStr;
+    }
+    @GetMapping("/welcome")
+    public String welecome() {
+        return welcomeStr;
     }
 }
